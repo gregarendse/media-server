10944d151b08cf1fb611ef3ea0f64558f3a0cd4d
Gregory Arendse <greg.arendse@gmail.com>
2022-10-29 08:12:18 +0000

    Added Tautulli

---
+++ upgrade.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit  # Used to exit upon error, avoiding cascading errors
+set -o pipefail # Unveils hidden failures
+# set -o xtrace   # Print commands and their arguments as they are executed.
+
+application="${1}"
+
+function help() {
+    echo """
+    Usage: ${0} <application>
+
+    Available applciations are: $(find applications -mindepth 1 -maxdepth 1 -type d -printf '%f ')
+    """
+    exit 1
+}
+
+[[ -z "${application}" ]] && help || echo "Application: ${application}"
+
+[[ ! -d "applications/${application}" ]] && help
+
+if [ ! -d "/home/docker/${application}" ];
+then
+    echo "No 'home' directory"
+    exit 1
+fi
+
+helm upgrade --install "${application}" server --values="applications/${application}/values.yaml" --namespace="${application}" --create-namespace --atomic
